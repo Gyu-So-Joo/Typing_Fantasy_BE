767c6a10a3925dee7ae64456bdb5c5b1380119fc
Ourumo <ss8638@naver.com>
2026-05-22 14:36:38 +0900

    Init Project

---
+++ src/main/java/com/GyuSoJoo/TypingFantasy/TypingFantasyApplication.java
@@ -0,0 +1,13 @@
+package com.GyuSoJoo.TypingFantasy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TypingFantasyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TypingFantasyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/GyuSoJoo/TypingFantasy/TypingFantasyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GyuSoJoo.TypingFantasy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TypingFantasyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
