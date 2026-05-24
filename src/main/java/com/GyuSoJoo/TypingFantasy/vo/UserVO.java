5daca918d1b6cca711bcf830d3d533b7f57c9f49
Ourumo <ss8638@naver.com>
2026-05-24 19:24:23 +0900

    Feat: UserVO 추가

---
+++ src/main/java/com/GyuSoJoo/TypingFantasy/vo/UserVO.java
@@ -0,0 +1,21 @@
+package com.GyuSoJoo.TypingFantasy.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.Map;
+
+@Data @NoArgsConstructor @AllArgsConstructor
+public class UserVO {
+    private Long id;
+    private String name;
+    private String password;
+    private String monsterIds;
+    private Map<String, Integer> recordTotalErrors;
+    private Long totalScore;
+    private BigDecimal recordAccuracyAvg;
+    private int recordCpmAvg;
+    private String createdAt;
+}
