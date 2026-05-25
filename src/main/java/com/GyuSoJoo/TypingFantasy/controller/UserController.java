612190de442714917afacdebc032908235ab69ef
Ourumo <ss8638@naver.com>
2026-05-25 10:54:29 +0900

    Feat: UserController 추가 | 기본적인 로그인, 회원가입 기능

---
+++ src/main/java/com/GyuSoJoo/TypingFantasy/controller/UserController.java
@@ -0,0 +1,34 @@
+package com.GyuSoJoo.TypingFantasy.controller;
+
+import com.GyuSoJoo.TypingFantasy.service.UserService;
+import com.GyuSoJoo.TypingFantasy.vo.UserVO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("api/user")
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/register")
+    public Map<String, Object> register(@RequestBody UserVO user) {
+        boolean is_success = userService.register(user);
+        if (!is_success) {
+            return Map.of("status", HttpStatus.CONFLICT.value(), "message", "중복된 아이디입니다.");
+        }
+        return Map.of("status", HttpStatus.CREATED.value(), "message", "회원가입 성공");
+    }
+
+    @GetMapping("/login")
+    public Map<String, Object> login(@RequestBody UserVO user) {
+        UserVO found = userService.login(user.getName(), user.getPassword());
+        if (found == null) {
+            return Map.of("status", HttpStatus.NOT_FOUND.value(), "message", "로그인 실패");
+        }
+        return Map.of("status", HttpStatus.OK.value(), "message", "로그인 성공", "name", found.getName());
+    }
+}
