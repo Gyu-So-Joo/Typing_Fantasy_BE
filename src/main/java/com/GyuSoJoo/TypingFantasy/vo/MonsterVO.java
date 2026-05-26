172534a8722ae1b3fd41c171d1d012c6286a2463
Ourumo <ss8638@naver.com>
2026-05-26 16:26:53 +0900

    Feat: MonsterVO 추가

---
+++ src/main/java/com/GyuSoJoo/TypingFantasy/vo/MonsterVO.java
@@ -0,0 +1,13 @@
+package com.GyuSoJoo.TypingFantasy.vo;
+
+public class MonsterVO {
+    private Long id;
+    private String name;
+    private int level;
+    private String detail;
+    private String jsCode;
+    private String javaCode;
+    private String silhouetteImg;
+    private String normalImg;
+    private String createdAt;
+}
