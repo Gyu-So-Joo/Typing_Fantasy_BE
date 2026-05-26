85b75aa83ac0c303af41e8533088818a441d51df
Ourumo <ss8638@naver.com>
2026-05-26 16:33:15 +0900

    Feat: MonsterVO에 lombok 어노테이션 추가

--- src/main/java/com/GyuSoJoo/TypingFantasy/vo/MonsterVO.java
+++ src/main/java/com/GyuSoJoo/TypingFantasy/vo/MonsterVO.java
@@ -1,5 +1,10 @@
 package com.GyuSoJoo.TypingFantasy.vo;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data @NoArgsConstructor @AllArgsConstructor
 public class MonsterVO {
     private Long id;
     private String name;
