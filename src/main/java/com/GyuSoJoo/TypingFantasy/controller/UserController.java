d2cf1f7848b782f257f9e82dc4d4b587022f2407
Ourumo <ss8638@naver.com>
2026-05-26 13:20:17 +0900

    Feat: SwaggerUI 기능 추가 | 유저에 우선 적용

--- src/main/java/com/GyuSoJoo/TypingFantasy/controller/UserController.java
+++ src/main/java/com/GyuSoJoo/TypingFantasy/controller/UserController.java
@@ -2,6 +2,11 @@ package com.GyuSoJoo.TypingFantasy.controller;
 
 import com.GyuSoJoo.TypingFantasy.service.UserService;
 import com.GyuSoJoo.TypingFantasy.vo.UserVO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -10,11 +15,17 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("api/user")
+@Tag(name = "User API", description = "유저 관련 API")
 public class UserController {
     @Autowired
     private UserService userService;
 
     @PostMapping("/register")
+    @Operation(summary = "회원가입")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "409", description = "중복된 아이디", content = @Content(mediaType = "application/json")),
+            @ApiResponse(responseCode = "201", description = "회원가입 성공", content = @Content(mediaType = "application/json"))
+    })
     public Map<String, Object> register(@RequestBody UserVO user) {
         boolean is_success = userService.register(user);
         if (!is_success) {
@@ -24,6 +35,11 @@ public class UserController {
     }
 
     @GetMapping("/login")
+    @Operation(summary = "로그인")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "404", description = "로그인 실패", content = @Content(mediaType = "application/json")),
+            @ApiResponse(responseCode = "200", description = "로그인 성공", content = @Content(mediaType = "application/json"))
+    })
     public Map<String, Object> login(@RequestBody UserVO user) {
         UserVO found = userService.login(user.getName(), user.getPassword());
         if (found == null) {
