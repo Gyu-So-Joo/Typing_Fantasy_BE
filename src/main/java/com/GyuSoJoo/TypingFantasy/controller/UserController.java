40423f8ebb36cec339c2880c293fc63cac32671c
Ourumo <ss8638@naver.com>
2026-05-29 02:56:24 +0900

    Rename: 변수명 is_success -> isSuccess로 변경

--- src/main/java/com/GyuSoJoo/TypingFantasy/controller/UserController.java
+++ src/main/java/com/GyuSoJoo/TypingFantasy/controller/UserController.java
@@ -2,9 +2,7 @@ package com.GyuSoJoo.TypingFantasy.controller;
 
 import com.GyuSoJoo.TypingFantasy.dto.ResponseObj;
 import com.GyuSoJoo.TypingFantasy.dto.UserDTO;
-import com.GyuSoJoo.TypingFantasy.service.UserRankingService;
 import com.GyuSoJoo.TypingFantasy.service.UserService;
-import com.GyuSoJoo.TypingFantasy.service.UserStatsService;
 import com.GyuSoJoo.TypingFantasy.vo.UserVO;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -16,7 +14,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
-import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("api/user")
@@ -25,12 +23,6 @@ import java.util.List;
 public class UserController {
     @Autowired
     private UserService userService;
-
-    @Autowired
-    private UserStatsService userStatsService;
-
-    @Autowired
-    private UserRankingService userRankingService;
 
     @PostMapping("/register")
     @ResponseStatus(HttpStatus.CREATED)
@@ -44,9 +36,9 @@ public class UserController {
         user.setName(request.name());
         user.setPassword(request.password());
 
-        boolean is_success = userService.register(user);
+        boolean isSuccess = userService.register(user);
 
-        if (!is_success) {
+        if (!isSuccess) {
             return ResponseObj.of(HttpStatus.CONFLICT.value(), "중복된 아이디입니다.");
         }
         return ResponseObj.of(HttpStatus.CREATED.value(), "회원가입 성공");
@@ -69,52 +61,4 @@ public class UserController {
         UserDTO.LoginResponse response = new UserDTO.LoginResponse(user.getId(), user.getName(), user.getMonsterIds(), user.getSelectedLang());
         return ResponseObj.of(HttpStatus.OK.value(), "로그인 성공", response);
     }
-
-    @GetMapping("/stats/{name}")
-    @ResponseStatus(HttpStatus.OK)
-    @Operation(summary = "유저 통계 조회")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "404", description = "조회 실패", content = @Content(mediaType = "application/json")),
-            @ApiResponse(responseCode = "200", description = "조회 성공", content = @Content(mediaType = "application/json"))
-    })
-    public ResponseObj<UserDTO.StatsResponse> getUserStats(@PathVariable("name") String name) {
-        UserVO user = userStatsService.getUserStats(name);
-
-        if (user == null) {
-            return ResponseObj.of(HttpStatus.NOT_FOUND.value(), "유저 통계 조회 실패");
-        }
-
-        UserDTO.StatsResponse response = new UserDTO.StatsResponse(
-                user.getRecordTotalErrors(),
-                user.getTotalScore(),
-                user.getRecordAccuracyAvg(),
-                user.getRecordCpmAvg()
-        );
-        return ResponseObj.of(HttpStatus.OK.value(), "유저 통계 조회 성공", response);
-    }
-
-    @GetMapping("/rank/list")
-    @ResponseStatus(HttpStatus.OK)
-    @Operation(summary = "유저 랭킹 조회")
-    @ApiResponse(responseCode = "200", description = "조회 성공", content = @Content(mediaType = "application/json"))
-    public ResponseObj<List<UserDTO.RankingsResponse>> getUserRankingList() {
-        return ResponseObj.of(HttpStatus.OK.value(), "유저 랭킹 조회 성공", userRankingService.getUserRankingList());
-    }
-
-    @PatchMapping("/{id}/lang/{selectedLang}")
-    @ResponseStatus(HttpStatus.OK)
-    @Operation(summary = "유저 언어 변경")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "404", description = "변경 실패", content = @Content(mediaType = "application/json")),
-            @ApiResponse(responseCode = "200", description = "변경 성공", content = @Content(mediaType = "application/json"))
-    })
-    public ResponseObj<Void> setSelectedLang(@PathVariable("id") long id, @RequestBody UserDTO.LangRequest request) {
-        boolean isSuccess = userService.setSelectedLang(id, request.selectedLang());
-
-        if (!isSuccess) {
-            return ResponseObj.of(HttpStatus.NOT_FOUND.value(), "유저 언어 변경 실패");
-        }
-        return ResponseObj.of(HttpStatus.OK.value(), "유저 언어 변경 성공");
-    }
-
 }
