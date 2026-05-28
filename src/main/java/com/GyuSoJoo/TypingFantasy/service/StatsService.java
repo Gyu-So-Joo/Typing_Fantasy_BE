36f5e914711a5c33069121972e263d2cf51b5d39
Ourumo <ss8638@naver.com>
2026-05-28 10:53:03 +0900

    Feat: StatsService 추가

---
+++ src/main/java/com/GyuSoJoo/TypingFantasy/service/StatsService.java
@@ -0,0 +1,17 @@
+package com.GyuSoJoo.TypingFantasy.service;
+
+import com.GyuSoJoo.TypingFantasy.mapper.UserMapper;
+import com.GyuSoJoo.TypingFantasy.vo.UserVO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class StatsService {
+    @Autowired
+    private UserMapper userMapper;
+
+    // 회원 통계
+    public UserVO userStats(String name) {
+        return userMapper.findByName(name);
+    }
+}
