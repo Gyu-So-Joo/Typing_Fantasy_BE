4cf566ed91e1338c4c95d07cf5199c99059e4a5d
Ourumo <ss8638@naver.com>
2026-05-29 09:04:33 +0900

    Feat: RecordVO 추가

---
+++ src/main/java/com/GyuSoJoo/TypingFantasy/vo/RecordVO.java
@@ -0,0 +1,23 @@
+package com.GyuSoJoo.TypingFantasy.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data @NoArgsConstructor @AllArgsConstructor
+public class RecordVO {
+    private long id;
+    private long userId;
+    private String userName;
+    private String selectedLang;
+    private int timer;
+    private BigDecimal accuracy;
+    private int cpm;
+    private int specialCharError;
+    private int caseMismatchError;
+    private int indentationError;
+    private int normalTextError;
+    private String createdAt;
+}
