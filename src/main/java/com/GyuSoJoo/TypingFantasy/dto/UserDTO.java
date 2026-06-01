85aaa396f0972f09de68311ff30afae5a1e32abb
Ourumo <ss8638@naver.com>
2026-06-01 13:39:01 +0900

    Remove: UserDTO의 StatsResponse 삭제 | 통계 관련 로직 변경

--- src/main/java/com/GyuSoJoo/TypingFantasy/dto/UserDTO.java
+++ src/main/java/com/GyuSoJoo/TypingFantasy/dto/UserDTO.java
@@ -10,8 +10,6 @@ public class UserDTO {
     public record LoginRequest(String name, String password) {}
     public record LoginResponse(Long id, String name, String selectedLang) {}
 
-    public record StatsResponse(String recordTotalErrors, Long totalScore, BigDecimal recordAccuracyAvg, int recordCpmAvg) {}
-
     public record RankingsResponse(String name, Long totalScore) {}
 
     public record LangRequest(Long id, String selectedLang) {}
